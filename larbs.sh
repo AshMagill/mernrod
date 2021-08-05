f142c47f3e61d5f83fad5b5c58afcd88f2728a06
ashm88 <ashmagill.newzealand@gmail.com>
2021-08-06 11:14:43 +1200

    added set backgound command

--- larbs.sh
+++ larbs.sh
@@ -191,6 +191,8 @@ newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 # Install everything on Nvim before its run 
 nvim --headless +PlugInstall +qall && nvim --headless +CocUpdate +qall
 
+setbg /home/thrasher/.local/share/thiemeyer_road_to_samarkand.jpg
+
 # Make pacman and yay colorful and adds eye candy on the progress bar because why not.
 grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
