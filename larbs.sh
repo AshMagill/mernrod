1b1b26166525f2ac21b989e6e20556981c5ed13f
ashm88 <ashmagill.newzealand@gmail.com>
2021-08-06 09:48:28 +1200

    install git plugs and coc before vim is opened

--- larbs.sh
+++ larbs.sh
@@ -189,8 +189,7 @@ newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 
 
 # Install everything on Nvim before its run 
-nvim --headless +PlugInstall +qall
-nvim --headless +CocUpdate +qall
+nvim --headless +PlugInstall +qall && nvim --headless +CocUpdate +qall
 
 # Make pacman and yay colorful and adds eye candy on the progress bar because why not.
 grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
