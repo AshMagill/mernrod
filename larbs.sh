6e7175f64f330cc0fea1c570e12c2f658e7a70a3
AshMagill <ashmagillnz@gmail.com>
2022-03-06 11:50:48 +1300

    save vim file dwm/config

--- larbs.sh
+++ larbs.sh
@@ -222,6 +222,7 @@ dialog --title "LARBS Installation" --infobox "Finally, installing \`libxft-bgra
 yes | sudo -u "$name" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
 
 #Update dwm changes
+vim ~/.local/src/dwm/config.h +wq &
 sudo make -C ~/.local/src/dwm install &>/dev/null 
 
 # Install the dotfiles in the user's home directory
