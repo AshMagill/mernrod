cd7a3b8bf92b22c5101d68babaf1e81fcc915b3f
AshMagill <ashmagillnz@gmail.com>
2022-03-19 13:30:41 +1300

    silent vim install

--- larbs.sh
+++ larbs.sh
@@ -233,7 +233,7 @@ https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA \"~
 https://www.archlinux.org/feeds/news/" > "/home/$name/.config/newsboat/urls"
 
 # run vim in the background to install everything (qietly)
-echo -ne '\n' | nvim
+echo -ne '\n' | nvim --headless
 
 # make git ignore deleted LICENSE & README.md files
 git update-index --assume-unchanged "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
