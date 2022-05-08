2f3a0e3fdce68a36457675b248185c77ce6c9f8b
AshMagill <ashmagillnz@gmail.com>
2022-05-08 14:08:14 +1200

    removed vs sh and put code directly in mernrod.sh

--- vscode-installer/CachedExtensionVSIXs/vs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-code --install-extension ./vscode-installer/CachedExtensionVSIXs/esbenp.prettier-vscode.vsix --enable-proposed-api ./vscode-installer/CachedExtensionVSIXs/esbenp.prettier-vscode --install-extension ./vscode-installer/CachedExtensionVSIXs/tabnine.tabnine-vscode.vsix --enable-proposed-api ./vscode-installer/CachedExtensionVSIXs/tabnine.tabnine-vscode.vsix --install-extension ./vscode-installer/CachedExtensionVSIXs/sainnhe.gruvbox-material.vsix --enable-proposed-api ./vscode-installer/CachedExtensionVSIXs/sainnhe.gruvbox-material.vsix --install-extension ./vscode-installer/CachedExtensionVSIXs/ms-vscode.references-view.vsix --enable-proposed-api ./vscode-installer/CachedExtensionVSIXs/ms-vscode.references-view.vsix --install-extension ./vscode-installer/CachedExtensionVSIXs/msjsdiag.debugger-for-chrome.vsix --enable-proposed-api ./vscode-installer/CachedExtensionVSIXs/msjsdiag.debugger-for-chrome.vsix --install-extension ./vscode-installer/CachedExtensionVSIXs/dsznajder.es7-react-js-snippets.vsix --enable-proposed-api ./vscode-installer/CachedExtensionVSIXs/dsznajder.es7-react-js-snippets.vsix && cp -f ./vscode-installer/settings.json ~/.config/Code/User
-
-
-
