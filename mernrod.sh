81e5c3226b10b136ccc5bfd61d428de8ef9841da
AshMagill <ashmagillnz@gmail.com>
2022-05-08 18:32:08 +1200

    added vscode settings.json

--- mernrod.sh
+++ mernrod.sh
@@ -225,9 +225,10 @@ sudo usermod -aG docker $name &>/dev/null
 #Install vscode extensions
 dialog --title "MERNROD Installation" --infobox "Installing VSCode extensions..." 5 70
 sudo -u "$name" code --install-extension esbenp.prettier-vscode --enable-proposed-api esbenp.prettier-vscode --install-extension tabnine.tabnine-vscode --enable-proposed-api tabnine.tabnine-vscode --install-extension sainnhe.gruvbox-material --enable-proposed-api sainnhe.gruvbox-material --install-extension ms-vscode.references-view --enable-proposed-api ms-vscode.references-view --install-extension msjsdiag.debugger-for-chrome --enable-proposed-api msjsdiag.debugger-for-chrome --install-extension dsznajder.es7-react-js-snippets --enable-proposed-api dsznajder.es7-react-js-snippets &>/dev/null 
+
 #Install vscode settings
 dialog --title "MERNROD Installation" --infobox "Installing VSCode settings..." 5 70
-putgitrepo "https://github.com/ashmagill/vscode/settings/main/settings.json" "/home/$name/.config/Code/User" &
+cp -f "https://github.com/ashmagill/vscode/settings/main/settings.json" "/home/$name/.config/Code/User" &
 
 #install emojis
 dialog --title "MERNROD Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
